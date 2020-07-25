2c62b716547be709ecbf1c895f5087b4293bad99
Tokki0401 <gyuhun95@gmail.com>
2020-07-25 12:28:30 -0700

    webpack babel setup. basic dependencies added.

---
+++ client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Hey there</div>
+      </div>
+    )
+  }
+}
+
+export default App;
