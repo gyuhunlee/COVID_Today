f9f6f69550f10aa7e7bc11b771f078eeff3ccc51
Tokki0401 <gyuhun95@gmail.com>
2020-07-27 14:50:03 -0700

    minor changes to App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 import TableDataList from './TableDataList.jsx';
+import SortBy from './SortBy.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -33,18 +34,31 @@ class App extends React.Component {
     var timeNow = selectedDate || new Date();
     var year = String(timeNow.getFullYear());
     var mm = String(timeNow.getMonth() + 1).padStart(2, '0');
-    var dd = String(timeNow.getDate()).padStart(2, '0');
+    var dd = String(timeNow.getDate() - 1).padStart(2, '0');
 
     return Number(year + mm + dd);
   }
 
+  tableTitle(date) {
+    date = date.toString().split('');
+    return date.reduce((acc, digit, index) => {
+      if (index === 4 || index === 6) {
+        acc += '/' + digit;
+      } else {
+        acc += digit;
+      }
+      return acc;
+    }, '');
+  }
+
   render() {
     var { covidData } = this.state;
-    var time = this.getTodayDate();
+    var time = this.tableTitle(this.getTodayDate());
     return (
       <div>
         <h1 id='title'>COVID-19 Cases and Deaths As Of {time}</h1>
-        <TableDataList covidData={this.state.covidData} />
+        <SortBy />
+        <TableDataList covidData={covidData} />
       </div>
     )
   }
---
+++ client/src/components/SortBy.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SortBy = (props) => {
+  return (
+    <div>Moo</div>
+  )
+}
+
+export default SortBy;
