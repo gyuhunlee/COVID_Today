4b6bd53bb7523100799ee1da86fd15b5ac4e43d9
Tokki0401 <gyuhun95@gmail.com>
2020-07-27 11:08:53 -0700

    time added

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import axios from 'axios';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      todayData: []
+    }
+  }
+
+  componentDidMount() {
+    getRequest(getTodayDate());
+  }
+
+  getRequest(today) {
+    const data = { today };
+    axios.get('/covid/domestic', data)
+    .then(prevCovidData => {
+      this.setState({
+        todayData: prevCovidData
+      })
+    })
+  }
+
+  getTodayDate() {
+    var timeNow = new Date();
+
+    var year = String(timeNow.getFullYear());
+    var mm = String(timeNow.getMonth() + 1).padStart(2, '0');
+    var dd = String(timeNow.getDate()).padStart(2, '0');
+
+    return Number(year + mm + dd);
   }
 
   render() {
